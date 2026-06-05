f8f6f47665f49ffd942fe26739bd871bf1c8a362
Ivan Diego Guimarães Lopes <ivandglopes@hotmail.com>
2026-06-04 22:51:26 -0300

    Inserido laços de repetição (ainda aprendendo)

--- src/generator.py
+++ src/generator.py
@@ -6,17 +6,32 @@ name = input("Enter your name: ")
 address = input("Enter your address: ")
 
 #Entrada de dados empresariais
-hired = input("Enter your hiring date: ")
+while True:
+    try:
+        hired = int(input("Enter your hiring date (DD/MM/YYYY): "))
+        break
+    except ValueError:
+        print("Only numbers!")
+
 title = input("Enter your job title: ")
 division = input("Enter your role: ")
 
-#Gerador de cartões (parte 1 = gerência)
-if division == "Operational":
-    part1 = "OPER"
-elif division == "Developer":
-    part1 = "DEV"
-elif division == "Security":
-    part1 = "SEC"
-else:
-    print("Insert valid role.")
+while True:
+    try:
+        if division == "Operational":
+            part1 = "OPER"
+        elif division == "Developer":
+            part1 = "DEV"
+        elif division == "Security":
+            part1 = "SEC"
+    except:
+        print("Insert valid role.")
+    break
 
+#Gerador de cartões (parte 1 = gerência)
+
+
+part2 = (str(hired)[::-1])
+
+#hired = 
+print(f"{part1}-{part2}")
